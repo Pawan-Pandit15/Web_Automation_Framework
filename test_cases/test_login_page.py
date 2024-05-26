bf042d2bb8a974231e1200558a26d88375a31446
Pawan <Pawanpandit332@gmail.com>
2024-05-26 13:47:11 +0530

    Framework

--- test_cases/test_login_page.py
+++ test_cases/test_login_page.py
@@ -5,6 +5,8 @@ import time
 import allure
 from page_objects.login_page import LoginPage  # here "login_page" is a file name and "LoginPage" is a class name
 from allure_commons.types import AttachmentType
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 @allure.title("Nopcommerce Login Test")
@@ -15,7 +17,7 @@ def test_nopcommerce_login_positive(setup):
     login_page = LoginPage(driver)
     login_page.login_to_nopcommerce(usr="admin@yourstore.com", pwd="admin")
     print("Title", driver.title)
-    time.sleep(5)
+    # WebDriverWait(driver, 10).until(EC.title_is("Dashboard / nopCommerce administration"))
     assert driver.title == "Dashboard / nopCommerce administration"
 
 
@@ -27,7 +29,7 @@ def test_logout(setup):
     login_page = LoginPage(driver)
     login_page.logout(usr="admin@yourstore.com", pwd="admin")
     print("Title", driver.title)
-    time.sleep(5)
+    # WebDriverWait(driver, 10).until(EC.title_is("Your store. Login"))
     assert driver.title == "Your store. Login"
 
 
